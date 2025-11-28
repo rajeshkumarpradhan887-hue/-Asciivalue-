1f29f5168b61b5b25d4652d8ce7decfae93384a9
Rajesh Kumar Pradhan <rajeshkumarpradhan887@gmail.com>
2025-11-28 20:27:24 +0530

    Add files via upload

---
+++ Asciivalue/Asciivalue.java
@@ -0,0 +1,10 @@
+class Asciivalue {
+    public static void main(String args[]){
+        char ch= 'A';
+        int ascii= ch;
+        int castAscii=(int) ch;
+        System.out.println("the Ascii value of "+ ch + " is:" + ascii );
+        System.out.println("the Ascii value of "+ ch  + " is:" + castAscii );
+
+    }
+}
