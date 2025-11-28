3d39cc2cbe12aff2c067aaef9d7b64b900b1b62d
Rajesh Kumar Pradhan <rajeshkumarpradhan887@gmail.com>
2025-11-28 19:19:28 +0530

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -1,28 +1,28 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
-class Apple{
-    public static void main(String args[]){
-        
+public class Apple {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean isPrime = true;
 
-        System.out.print("enter the number");
-         int num=sc.nextInt();
         if (num <= 1) {
-            !flag = false;  
+            isPrime = false;
         } else {
             for (int i = 2; i <= num / 2; i++) {
                 if (num % i == 0) {
-                    
+                    isPrime = false;
                     break;
                 }
             }
         }
 
-        if (!flag)
+        if (isPrime)
             System.out.println(num + " is a prime number.");
         else
             System.out.println(num + " is not a prime number.");
+
+        sc.close();
     }
 }
-
-
